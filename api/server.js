01871caeb460b65c163aa333e91e58a56082fb49
carlcook706 <72904567+carlcook706@users.noreply.github.com>
2022-07-29 07:26:52 +0800

    up

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+require('dotenv').config()
+
 const app = express();
 
 app.use(express.json());
@@ -54,10 +56,10 @@ app.put('/todo/update/:id', async (req, res) => {
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect('mongodb+srv://carlcook706:Patpat12@cluster0.aw03ggg.mongodb.net/?retryWrites=true&w=majority', {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
-}).then(app.listen(PORT,()=>{console.log(`Server running on port ${PORT} and Connected to DB`)}));
+}).then(app.listen(process.env.PORT,()=>{console.log(`Server running on port ${PORT} and Connected to DB`)}));
 
